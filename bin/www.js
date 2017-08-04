63a5d493315b32752ed70fbd1465eb0dd9a3794f
Christian Rich <christian.schlosrich@gmail.com>
2017-08-04 22:20:12 +1000

    Spawning child workers (multi threading support)

--- bin/www.js
+++ bin/www.js
@@ -1,18 +1,41 @@
 import app from '+/api';
+import os from 'os';
+import cluster from 'cluster';
 import { default as swagger } from '../api/swagger/util';
-
-console.log('/bin/www');
 
 (async() => {
 
 	try{
-        await swagger.init(app);
+		await swagger.init(app);
 	} catch(e){
-		console.log('Errors during app startup proc');
 		return console.log(e);
 	}
 
-	app.server.listen(process.env.PORT || 3000, () => {
-		console.log(`Express server started on port ${app.server.address().port}`);
-	});
+	const numCPUs = os.cpus().length;
+	console.log(`Starting ${numCPUs} clusters`);
+
+	if (cluster.isMaster) {
+
+		console.log(`Master ${process.pid} is running`);
+
+		for (let i = 0; i < numCPUs; i++) {
+			cluster.fork();
+		}
+
+		cluster.on('exit', (worker, code, signal) => {
+			console.log(`worker ${worker.process.pid} died`);
+		});
+
+		cluster.on('online', (worker, code, signal) => {
+			console.log(`worker ${worker.process.pid} online`);
+		});
+
+		cluster.on('listening', (worker, code, signal) => {
+			console.log(`worker ${worker.process.pid} listening`);
+		});
+	} else {
+		app.server.listen(process.env.PORT || 3000, () => {
+			console.log(`Express server started on port ${app.server.address().port}`);
+		});
+	}
 })();
